938d59550e0ef2eec077ea855f9bd61b130494c3
David Richardson <david@tankski.co.uk>
2020-03-23 10:26:50 +0000

    Add some more comments

--- rounds/base.py
+++ rounds/base.py
@@ -93,7 +93,7 @@ class TargetFace(object):
     (e.g.: diameter, number of scoring rings) that can be leveraged for
     calculating handicaps for the round. 
 
-    Parameters
+    Attributes
     ----------
     name : str
         The name of this target face, e.g.: "40cm 10 zone".
@@ -137,11 +137,32 @@ class TargetFace(object):
 
 
 class SubRound(object):
-    """ """
+    """A specific distance of a `Round`.
+
+    Each distance in a round is considered a sub-round, each with information 
+    about how many arrows are shot, the `TargetFace` it's shot on, and the 
+    distance it's shot at. A `Round` is formed of at at least one `SubRound`.
+
+    Attributes
+    ----------
+    num_arrows : int
+        The number of arrows shot at this distance.
+    distance : float
+        The distance that this SubRound is shot at in metres.
+    distance_type : :obj:`DistanceType`
+        The units of the distance this round is shot at. This is used when 
+        displaying the distance in a formatted way.
+    target_face : :obj:`TargetFace`
+        The target face that this SubRound is shot at.
+    scoring_type : :obj:`ScoringType`
+        The superficial scoring system that this SubRound uses. This does not 
+        affect how handicaps are calculated.
+    
+    """
 
     def __init__(
         self,
-        num_arrows: str,
+        num_arrows: int,
         distance: float,
         scoring_type: ScoringType,
         target_face: TargetFace,
@@ -154,16 +175,26 @@ class SubRound(object):
         self.distance_type = distance_type
 
     def readable_distance(self, decimal_places: int = 2) -> str:
-        """
+        """Returns the distance for this `SubRound` in human readable form
 
-        :param decimal_places: int:  (Default value = 2)
+        Parameters
+        ----------
+        decimal_places : int, optional
+            The number of decimal places to format the distance to. Defaults 
+            to 2.
+
+        Returns
+        -------
+        str
+            The distance with units, formatted the given number of decimal 
+            places (or 2 by default).
 
         """
         if self.distance_type is DistanceType.METRES:
             return (
                 "{val}m".format(val=round(self.distance, decimal_places))
-                .rstrip("0")
-                .rstrip(".")
+                # rstrip is used
+                .rstrip("0").rstrip(".")
             )
         else:
             return (
@@ -194,9 +225,34 @@ class SubRound(object):
 
 
 class Round(object):
-    """ """
+    """A scoring round made up of at least one sub round.
+    
+    Rounds must be made up of at least 1 `SubRound`. It may contain a number of 
+    identical sub rounds (e.g.: WA 18m) or rounds descending order of distance. 
+    
+    Two rounds are said to be comparable if they are in the same `RoundClass`.
+
+    Attributes
+    ----------
+    name : str
+        The name of this round e.g.: "St. Nicholas".
+    season : :obj:`Season`
+        The season that this round is shot in.
+    subrounds : :obj:`list` of :obj:`SubRound`
+        The `SubRound`s that form up this archery round. Must contain at least 
+        `SubRound`
+
+    Raises
+    ------
+    ValueError
+        if `subrounds` is Falsy or otherwise doesn't contain at least one `SubRound`.
+    
+    """
 
     def __init__(self, name: str, season: Season, subrounds: List[SubRound]):
+        if not subrounds or len(subrounds) < 1:
+            raise ValueError("There must be at least 1 SubRound to form a round.")
+
         self.name = name
         self.season = season
         self.subrounds = subrounds
@@ -217,47 +273,104 @@ class Round(object):
 
     @property
     def num_arrows(self) -> int:
-        """ """
+        """Returns the total number of arrows shot across all `SubRound`s.
+        
+        Returns
+        -------
+        int
+            The total number of arrows shot across all subrounds.
+
+        """
         return reduce(lambda x, y: x + y.num_arrows, self.subrounds, 0)
 
     @property
     def distances(self) -> List[float]:
-        """ """
+        """Returns a list of distances in metres for each of the `SubRound`s 
+        in this `Round`.
+        
+        Returns
+        -------
+        :obj:`list` of float
+            A list of the distances for each `SubRound` in metres.
+
+        """
         return list(map(lambda x: x.distance, self.subrounds))
 
     def readable_distances(self, decimal_places: int = 2) -> List[str]:
-        """
+        """Returns a list of distances for each `SubRound`, formatted using
+        the appropriate units (yards or metres).
 
-        :param decimal_places: int:  (Default value = 2)
+        Parameters
+        ----------
+        decimal_places : int, optional
+            The number of decimal places to round the distance to, defaults to 
+            2 d.p.
+        
+        Returns
+        -------
+        :obj:`list` of :obj:`str`
+            A list of the distances for each `SubRound` with the associated units.
 
         """
         return list(map(lambda x: x.readable_distance(decimal_places), self.subrounds))
 
     def belongs_to(self, round_class: "RoundClass") -> bool:
-        """
+        """Determines if this `Round` is part of the given `RoundClass`.
 
-        :param round_class: "RoundClass": 
+        A `Round` belongs to a `RoundClass` if its subrounds are a valid permutation 
+        of the `RoundClass`. An example of this would be the WA 1440 Gents round 
+        class. This class defines that the 50m and 30m distances are valid if they're
+        shot with a 10 zone or 6 zone face.
+
+        This method is the equivalent of calling `RoundClass.validate_round` with 
+        this `Round` instance.
+
+        Parameters
+        ----------
+        round_class : :obj:`RoundClass`
+            The `RoundClass` to check membership of.
+
+        Returns
+        -------
+        bool
+            True if this `Round` belongs to the given `RoundClass`, False otherwise.
 
         """
         return round_class.validate_round(self)
 
 
-def valid_subround_choices(round_class: "RoundClass", r: "Round") -> bool:
-    """
+def valid_subround_choices(round_class: "RoundClass", rnd: "Round") -> bool:
+    """A `RoundValidator` function that checks that each `SubRound` in the 
+    given `Round` instance is in the corresponding `RoundClass`.
 
-    :param round_class: "RoundClass": 
-    :param r: "Round": 
+    Notes
+    -----
+        The ordering for the `SubRounds` matters. Ensure that the ordering is 
+        preserved between both `RoundClass` and `Round`.
+
+    Parameters
+    ----------
+    round_class : :obj:`RoundClass`
+        The `RoundClass` that contains the valid `SubRound` permutations.
+    rnd : :obj:`Round`
+        The `Round` to determine if its `SubRound`s are valid.
 
     """
     return reduce(
         lambda x, y: x and y,
-        [a in b for (a, b) in zip(r.subrounds, round_class.subrounds)],
+        [a in b for (a, b) in zip(rnd.subrounds, round_class.subrounds)],
         True,
     )
 
 
 class RoundClass(object):
-    """ """
+    """A set of `Round`s that can be considered the same.
+    
+    Attributes
+    ----------
+        
+
+    """
 
     def __init__(
         self,
